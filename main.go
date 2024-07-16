e02943a00ad0bebb02c348f03a4b9ce197a15ff8
dangkhuong14 <f13file31@gmail.com>
2024-07-16 15:17:07 +0700

    feat: create product api using gin

--- main.go
+++ main.go
@@ -3,47 +3,54 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 
+	"database/sql/driver"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"database/sql/driver"
 )
 
-// `id` int NOT NULL AUTO_INCREMENT,
-// `category_id` int DEFAULT NULL,
-// `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-// `image` json DEFAULT NULL,
-// `type` enum('drink','food','topping') NOT NULL DEFAULT 'drink',
-// `status` enum('activated','deactivated','out_of_stock') DEFAULT 'activated',
-// `description` text,
-// `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-// `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
 type BaseModel struct {
-	ID UUID `gorm:"column:id"`
-	Status string `gorm:"column:status"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	ID        UUID      `gorm:"column:id" json:"id"`
+	Status    string    `gorm:"column:status" json:"status"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 type Product struct {
 	BaseModel
-	CategoryID int `gorm:"column:category_id"`
-	Name string `gorm:"column:name"`
-	Image *string `gorm:"column:image"`
-	Type string `gorm:"column:type"`
-	Description string `gorm:"column:description"`
+	CategoryID  int     `gorm:"column:category_id" json:"category_id"`
+	Name        string  `gorm:"column:name" json:"name"`
+	Image       *string `gorm:"column:image" json:"image"`
+	Type        string  `gorm:"column:type" json:"type"`
+	Description string  `gorm:"column:description" json:"description"`
 }
 
 type ProductUpdate struct {
-	CategoryID *int `gorm:"column:category_id"`
-	Name *string `gorm:"column:name"`
-	Image *string `gorm:"column:image"`
-	Type *string `gorm:"column:type"`
-	Description *string `gorm:"column:description"`
-	Status *string `gorm:"column:status"`
+	CategoryID  *int    `gorm:"column:category_id" json:"category_id"`
+	Name        *string `gorm:"column:name" json:"name"`
+	Image       *string `gorm:"column:image" json:"image"`
+	Type        *string `gorm:"column:type" json:"type"`
+	Description *string `gorm:"column:description" json:"description"`
+	Status      *string `gorm:"column:status" json:"status"`
+}
+
+func GenNewBaseModel() BaseModel {
+	now := time.Now().UTC()
+	newUUID, _ := uuid.NewV7()
+
+	return BaseModel{
+		ID:        UUID(newUUID),
+		Status:    "activated",
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
 }
 
 func (Product) TableName() string {
@@ -70,13 +77,80 @@ func (id UUID) Value() (driver.Value, error) {
 
 // Method chuyển UUID từ byte slice thành chuỗi
 func (id UUID) String() string {
-    return uuid.UUID(id).String()
+	return uuid.UUID(id).String()
 }
+
+// func main() {
+// 	// Checking that an environment variable is present or not.
+// 	mysqlConnStr, ok := os.LookupEnv("MYSQL_CONNECTION")
+
+// 	if !ok {
+// 		log.Fatalln("Missing MySQL connection string")
+// 	}
+
+// 	dsn := mysqlConnStr
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatalln("Cannot connect to MySQL:", err)
+// 	}
+
+// 	log.Println("Connected to MySQL:", db)
+
+// 	// Create new Product instance
+// 	now:= time.Now().UTC()
+// 	productUUID, _ := uuid.NewV7()
+
+// 	newProduct := Product{
+// 		BaseModel: BaseModel{
+// 			ID: UUID(productUUID),
+// 			Status: "activated",
+// 			CreatedAt: now,
+// 			UpdatedAt: now,
+// 		},
+// 		CategoryID: 1,
+// 		Name: "Americano",
+// 		Image: nil,
+// 		Type: "drink",
+// 	}
+
+// 	if err := db.Table(Product{}.TableName()).Create(&newProduct).Error; err != nil {
+// 		log.Println("Cannot create new Product:", err)
+// 	} else {
+// 		log.Println("Product created:", newProduct)
+// 	}
+
+// 	// // Read Product instance
+// 	// var existingProduct Product
+
+// 	// if err:=db.
+// 	// Table(Product{}.TableName()).
+// 	// Where("id= ?", 1).
+// 	// First(&existingProduct).Error; err !=nil {
+// 	// 	log.Println("Cannot read from Product:", err)
+// 	// } else {
+// 	// 	log.Println("Product read:", existingProduct)
+// 	// }
+
+// 	//List products
+// 	var products []Product
+
+// 	if err:=db.
+// 	Table(Product{}.TableName()).
+// 	Where("status not in (?)", []string{"deactivated"}).
+// 	Limit(3).
+// 	Order("id desc").
+// 	Offset(0).
+// 	Find(&products).Error; err !=nil {
+// 		log.Println("Cannot list from Product:", err)
+// 	} else {
+// 		log.Println("Products listed:", products[0].ID)
+// 		log.Println("Products listed:", products[0].ID.String())
+// 	}
 
 func main() {
 	// Checking that an environment variable is present or not.
 	mysqlConnStr, ok := os.LookupEnv("MYSQL_CONNECTION")
-
 	if !ok {
 		log.Fatalln("Missing MySQL connection string")
 	}
@@ -87,101 +161,54 @@ func main() {
 	if err != nil {
 		log.Fatalln("Cannot connect to MySQL:", err)
 	}
-
 	log.Println("Connected to MySQL:", db)
 
-	// Create new Product instance
-	now:= time.Now().UTC()
-	productUUID, _ := uuid.NewV7()
+	// Gin API Ping
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
-	newProduct := Product{
-		BaseModel: BaseModel{
-			ID: UUID(productUUID),
-			Status: "activated",
-			CreatedAt: now,
-			UpdatedAt: now,
-		},
-		CategoryID: 1,
-		Name: "Americano",
-		Image: nil,
-		Type: "drink",
-	}
-	
-	if err := db.Table(Product{}.TableName()).Create(&newProduct).Error; err != nil {
-		log.Println("Cannot create new Product:", err)
-	} else {
-		log.Println("Product created:", newProduct)
-	}
+	// Create Product
+	v1 := r.Group("/v1")
+	products := v1.Group("products")
+	products.POST("", func(c *gin.Context) {
+		// Parse product data from body
+		var productData Product
 
-	// // Read Product instance
-	// var existingProduct Product
+		if err := c.Bind(&productData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 
-	// if err:=db.
-	// Table(Product{}.TableName()).
-	// Where("id= ?", 1).
-	// First(&existingProduct).Error; err !=nil {
-	// 	log.Println("Cannot read from Product:", err)
-	// } else {
-	// 	log.Println("Product read:", existingProduct)
-	// }
+		// Business's logic
+		productData.Name = strings.TrimSpace(productData.Name)
+		if productData.Name == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "product's name can not be blank",
+			})
+			return
+		}
 
-	//List products
-	var products []Product
+		productData.BaseModel = GenNewBaseModel()
 
-	if err:=db.
-	Table(Product{}.TableName()).
-	Where("status not in (?)", []string{"deactivated"}).
-	Limit(3).
-	Order("id desc").
-	Offset(0).
-	Find(&products).Error; err !=nil {
-		log.Println("Cannot list from Product:", err)
-	} else {
-		log.Println("Products listed:", products[0].ID)
-		log.Println("Products listed:", products[0].ID.String())
-	}
+		// Save to database
+		if err := db.Table(Product{}.TableName()).Create(&productData).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "can not create new product",
+			})
+			return
+		} else {
+			log.Println("Product created:", productData)
+			c.JSON(http.StatusCreated, gin.H{
+				"data": productData.ID.String(),
+			})
+		}
+	})
 
-	// // Update product 1: use existing product
-	// existingProduct.Name = "Latte"
-
-	// if err:=db.
-	// Table(Product{}.TableName()).
-	// Updates(existingProduct).Error; err !=nil {
-	// 	log.Println("Cannot update existing Product:", err)
-	// } else {
-	// 	log.Println("Products updated:", existingProduct)
-	// }
-
-	// // Update product 2: use where condition
-	// if err:=db.Table(Product{}.TableName()).
-	// Where("id = ?", 2).Updates(map [string]interface{}{"name": "Espresso"}).
-	// Error; err !=nil {
-	// 	log.Println("Cannot update Product with ID 2:", err)
-	// } else {
-	// 	log.Println("Products ID 2 updated:")
-	// }
-	
-	// // Update product 3: empty string
-	// emptyString := ""
-	// if err := db.
-	// Table(Product{}.TableName()).
-	// Where("id = ?", 3).
-	// Updates(ProductUpdate{Name: &emptyString}).Error; err !=nil {
-	// 	log.Println("Cannot update Product with ID 3:", err)
-	// } else {
-	// 	log.Println("Products ID 3 updated:")
-	// }
-
-	// // Delete product
-	// if err:=db.Table(Product{}.TableName()).
-	// Where("id = ?", 2).
-	// Delete(nil).Error; err !=nil {
-	// 	log.Println("Cannot delete Product with ID 2:", err)
-	// } else {
-	// 	log.Println("Products ID 2 deleted:")
-	// }
-
-	// UUID gen
-	newUUID, _ := uuid.NewV7()
-	log.Println(newUUID)
+	r.Run(":3000")
 }
