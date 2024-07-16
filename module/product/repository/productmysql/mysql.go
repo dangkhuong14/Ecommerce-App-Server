6b29d7bad1bc421d05e15ebdb3449bca816ba5bc
dangkhuong14 <f13file31@gmail.com>
2024-07-16 18:21:24 +0700

    feat: implement product repository adapter

---
+++ module/product/repository/productmysql/mysql.go
@@ -0,0 +1,15 @@
+package productmysql
+
+import (
+	"gorm.io/gorm"
+)
+
+type MysqlRepository struct {
+	db *gorm.DB
+}
+
+func NewMysqlRepository(db *gorm.DB) MysqlRepository {
+	return MysqlRepository{
+		db: db,
+	}
+}
